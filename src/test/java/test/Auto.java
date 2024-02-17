64e0b1aee21da95fc2df07c65c1992d766e7657a
DanielFernandoVerdecia <123104610+DanielFernandoVerdecia@users.noreply.github.com>
2024-02-17 11:39:16 -0500

    cuarto commit :,(

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -14,7 +14,23 @@ public class Auto {
     public int cantidadCreados;
 	
 	public int cantidadAsientos() {
-		return this.asientos.length;
+		
+		int asientos = 0;
+		
+		for (int i = 0; i < this.asientos.length; i++) {
+			
+			
+			if (this.asientos[i] != null) {
+				
+				asientos ++;
+				
+			}
+			
+			
+		}
+		
+		return asientos;
+		
 	}
 	
 	
@@ -22,39 +38,25 @@ public class Auto {
    public String verificarIntegridad() {
 	   
 
-	   //Registro auto
-	   int r_auto = this.registro;
-	   
-
-	   //Registro Asiento
-	   int r_asiento = new Asiento().registro;
-	 
-	   
-	   //Registro Motor
-	   int r_motor = new Motor().registro;
-	   
-	   
-	   //Validar que el registro no sea pirata
-	   if (r_auto != r_asiento) {
-		   
-		   
-		   return "Las piezas no son originales";
-		   
-	   }
-	   
-	   else if (r_auto != r_motor) {
-		   
-		   
-		   return "Las piezas no son originales";
-		   
-	   }
-	   
-	   else {
-		   
-		   return "Auto original";
-		   
-	   }
-	   
+	  if (this.registro == this.motor.registro) {
+		  
+		  for (int i = 0; i < this.asientos.length; i++) {
+			  
+			  if (this.asientos[i] != null && this.asientos[i].registro != this.registro) {
+				  
+				  return "Las piezas no son originales";
+				  
+			  }
+		  }
+		  
+		  return "Auto original";
+		  
+	  }
+	  
+	  else {
+		  
+		  return "Las piezas no son originales";
+	  }
 	  
 	   
    	}
